96a7cc686b303b4b0cf6c587267949cf5baf7a7c
Godwin45 <godwinmuthomi12@gmail.com>
2023-12-19 15:06:19 +0300

    data ingestion complete

---
+++ main.py
@@ -0,0 +1,14 @@
+from engineProject import logger
+from engineProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "DAta Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
